e35f4fea02c3b1de69bafe0fba5dce2f6ddb3fe5
Supakit <beam0820234866@gmail.com>
2024-07-14 15:04:07 +0700

    atm

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '2asfdqegqegqgqwqwdwvbvjvcg'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/atm'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Account(db.Model):
+    account_number = db.Column(db.String(50), primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    balance = db.Column(db.Float, nullable=False)
+
+    def __init__(self, account_number, username, balance):
+        self.account_number = account_number
+        self.username = username
+        self.balance = balance
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    accounts = Account.query.all()
+    return render_template('index.html', accounts=accounts)
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        account_number = request.form['account_number']
+        username = request.form['username']
+        balance = float(request.form['balance'])
+
+        new_account = Account(account_number, username, balance)
+        db.session.add(new_account)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('create_account.html')
+
+@app.route('/account/<account_number>', methods=['GET', 'POST'])
+def account(account_number):
+    account = Account.query.filter_by(account_number=account_number).first()
+    if not account:
+        return 'Account not found', 404
+
+    if request.method == 'POST':
+        action = request.form['action']
+        amount = float(request.form['amount'])
+
+        if action == 'deposit':
+            account.balance += amount
+        elif action == 'withdraw' and account.balance >= amount:
+            account.balance -= amount
+        else:
+            return 'Invalid operation', 400
+
+        db.session.commit()
+        return redirect(url_for('account', account_number=account_number))
+
+    return render_template('account.html', account=account)
+
+@app.route('/delete_account/<account_number>', methods=['POST'])
+def delete_account(account_number):
+    account = Account.query.filter_by(account_number=account_number).first()
+    if account:
+        db.session.delete(account)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
